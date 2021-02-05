331f6d1f9bd034e900d80b416ffc39775eab04a5
Zahro25 <zahro248120@gmail.com>
2021-02-05 13:38:53 +0700

    perulangan for while dan do while

---
+++ JS dasar/js/perulangan.js
@@ -0,0 +1,18 @@
+//perulangan for
+for(i = 1; i <= 10 ; i++){
+    console.log("perulangan for ke "+ i);
+}
+
+//perulangan while
+j = 1;
+while(j<= 10){
+    console.log("perulangan while ke "+ j);
+    j++;
+}
+
+//perulangan do while
+k=1;
+do{
+    console.log("perulangan do while "+k);
+    k++;
+}while(k <= 10);
